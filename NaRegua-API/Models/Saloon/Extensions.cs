b2dbeb113ed2b17714c4aae7b4645ab559211da4
Alex Silva <alex.silva@intelitrader.com.br>
2022-07-16 13:24:08 -0300

    add objeto saloon e seus requests e responses

---
+++ NaRegua-API/Models/Saloon/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using NaRegua_API.Common.Contracts;
+using System.Linq;
+using static NaRegua_API.Models.Professional.Requests;
+using static NaRegua_API.Models.Saloon.Responses;
+
+namespace NaRegua_API.Models.Saloon
+{
+    public static class Extensions
+    {
+        public static ListSaloonsResponse ToResponse(this ListSaloonsResult input)
+        {
+            return new ListSaloonsResponse
+            {
+                Resources = input.Resources.Select(item => item.ToDomain())
+            };
+        }
+
+        public static SaloonsResponse ToDomain(this SaloonsResult input)
+        {
+            return new SaloonsResponse
+            {
+                SaloonCode = input.SaloonCode,
+                Address = input.Address,
+                Name = input.Name,
+                Contact = input.Contact
+            };
+        }
+    }
+}
