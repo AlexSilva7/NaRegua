c9e3e27cb0b9d4e95a207de574ce4cebae55f667
Alex Silva <alex.silva@intelitrader.com.br>
2022-09-12 00:01:45 -0300

    add inserts sql

---
+++ NaRegua/Solution Items/SQL/INSERT.sql
@@ -0,0 +1,28 @@
+
+
+
+
+
+INSERT INTO SALOONS 
+VALUES ('ST2UH97QJ', 'Travessa União, nº9', 'Salão Top Hair', '(21) 97854-3349');
+
+INSERT INTO SALOONS 
+VALUES ('XP90TH364', 'Entrada da Rua 1, nº 399', 'Salão Só os Crias', '(21) 99685-0000');
+
+INSERT INTO SALOONS 
+VALUES ('97LQA69VC', 'Casa da paz, nº 500', 'Mega Estilo', '(21) 978543349');
+
+INSERT INTO SALOONS 
+VALUES ('788TGG90Q', 'Estrada da gávea, nº 420', 'Salão Bem Bolado', '(21) 97323-1111');
+
+INSERT INTO SALOONS 
+VALUES ('H48FA4DA0', 'Travessa Palmas, nº 240', 'Fashion Hair', '(21) 90000-8888');
+
+INSERT INTO SALOONS 
+VALUES ('HTR84NF09', 'Travessa Roma, nº 5100', 'Salão tô em casa', '(21) 91414-9999');
+
+INSERT INTO SALOONS 
+VALUES ('77PLH203A0', 'Rampa da vila verde, nº 99', 'Estilo 7', '(21) 91313-2222');
+
+INSERT INTO SALOONS 
+VALUES ('32RHHJB010', 'Rua dionéia, nº 15', 'Clube dos belos', '(21) 99999-0000');
