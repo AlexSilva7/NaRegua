3f2b3759dfbe2adb105aebde80309b885c0882bb
Alex Silva <alex.silva@intelitrader.com.br>
2022-10-08 21:53:35 -0300

    ajuste classes em pastas

--- NaRegua/NaRegua_Api/Repository/ActiveSessionRepository/AuthRepositorySqlServer.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NaRegua_Api.Repository.ActiveSessionRepository
-{
-    public class AuthRepositorySqlServer : AuthRepository
-    {
-        protected override string SELECT_CREDENTIALS_USER =>
-            "SELECT DISTINCT * FROM USERS WHERE USERNAME = @USERNAME";
-    }
-}
--- NaRegua/NaRegua_Api/Repository/ActiveSessionRepository/SaloonRepositorySqlServer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NaRegua_Api.Repository.ActiveSessionRepository
-{
-    public class SaloonRepositorySqlServer : SaloonRepository
-    {
-        protected override string SELECT_SALOONS => "SELECT DISTINCT * FROM SALOONS";
-
-        protected override string SELECT_SALOON_BY_CODE => "SELECT DISTINCT * FROM SALOONS WHERE CODE = @CODE";
-    }
-}
